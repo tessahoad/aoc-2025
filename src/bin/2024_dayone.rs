74678230fe917320e830a48e41a1ef2a493fb4e8
Tessa Hoad <contact@tessahoad.co.uk>
2025-12-01 12:42:00 +0000

    2024 day two progress

--- src/bin/2024_dayone.rs
+++ src/bin/2024_dayone.rs
@@ -1,4 +1,6 @@
-static INPUT: &str = include_str!("../input/dayone.txt");
+use itertools::Itertools;
+
+static INPUT: &str = include_str!("../input/2024_dayone.txt");
 
 fn split_string(string: &str) -> (&str, &str) {
     return match string.split_once("   ") {
@@ -11,7 +13,7 @@ fn int_diff((i1, i2): (&i32, &i32)) -> i32 {
     return (i1 - i2).abs();
 }
 
-fn read_lines() {
+fn part_one() {
     let split_lines = INPUT.lines().map(split_string);
     let mut first_locations: Vec<i32> = split_lines
         .clone()
@@ -26,10 +28,26 @@ fn read_lines() {
 
     let diff_list= first_locations.iter().zip(second_locations.iter()).map(int_diff);
     let total: i32 = diff_list.sum();
-    println!("Sum : {}", total)
+    println!("Part One Solution: {}", total)
+}
+
+fn part_two() {
+    let split_lines = INPUT.lines().map(split_string);
+    let mut first_locations: Vec<i32> = split_lines
+        .clone()
+        .map(|x| x.0.parse::<i32>().unwrap()).collect();
+    let second_locations = split_lines
+        .clone()
+        .map(|x| x.1.parse::<i32>().unwrap())
+        .chunk_by(|key| key);
+
+    let diff_list= first_locations.iter().zip(second_locations.iter()).map(int_diff);
+    let total: i32 = diff_list.sum();
+    println!("Part One Solution: {}", total)
 }
 
 
 fn main() {
-    read_lines();
+    part_one();
+    part_two();
 }
