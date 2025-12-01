879c8a76c37afff23e2e2cdecc01d099909ef416
Tessa Hoad <contact@tessahoad.co.uk>
2025-12-01 17:40:51 +0000

    Use Rust FromStr trait

--- src/bin/dayone.rs
+++ src/bin/dayone.rs
@@ -1,3 +1,5 @@
+use std::{str::FromStr};
+
 static INPUT: &str = include_str!("../input/dayone.txt");
 
 // struct for dial
@@ -20,13 +22,16 @@ impl Dial {
 }
 
 // enum for instruction
+#[derive(PartialEq, Debug)]
 pub enum Instruction {
     RotateLeft { degree: i32 },
     RotateRight { degree: i32 },
 }
 
-impl Instruction {
-    fn from_str(s: &str) -> Result<Instruction, &str> {
+impl FromStr for Instruction {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         let first = s.chars().next().ok_or("Empty string was passed")?;
         let tail = &s[first.len_utf8()..];
 
@@ -46,7 +51,7 @@ impl Instruction {
 }
 
 
-fn main() -> Result<(), String> {
+fn part_one() -> Result<(), String> {
     let instructions: Vec<Instruction> = INPUT.lines()
         .map(|line| Instruction::from_str(line))
         .collect::<Result<_, _>>()?;
@@ -62,11 +67,65 @@ fn main() -> Result<(), String> {
 
     let zeros = states.iter().filter(|d| d.position == 0).count();
     println!("Position is 0 {} times", zeros);
-    
-    // for instr in &instructions {
-    //     dial.apply(instr);
-    // }
 
     println!("Final dial position: {}", dial.position);
     Ok(())
+}
+
+
+fn main() -> Result<(), String> {
+    return part_one();
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_instruction_from_str_for_rotate_left() {
+        // Given
+        let input = "L32";
+        let expected = Ok(Instruction::RotateLeft { degree: 32 });
+        
+        // When
+        let actual = Instruction::from_str(input);
+        
+        // Then
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_instruction_from_str_for_rotate_right() {
+        // Given
+        let input = "R250";
+        let expected = Ok(Instruction::RotateRight { degree: 250 });
+
+        // When
+        let actual = Instruction::from_str(input);
+
+        // Then
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_instruction_from_str_fails_when_not_l_or_r() {
+        // Given
+        let input = "H123";
+        
+        // When
+        // Then
+        assert_eq!(Instruction::from_str(input), Err("Unexpected instruction character"))
+    }
+
+    #[test]
+    fn test_instruction_from_str_fails_when_bad_number() {
+        // Given
+        let input = "L23P";
+        
+        // When
+        // Then
+        assert_eq!(Instruction::from_str(input), Err("Failed to parse degree"))
+    }
+
 }
