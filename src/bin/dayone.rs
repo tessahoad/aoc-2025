b06a58bbbf919c92f01ed73068e4020443a7505c
Tessa Hoad <contact@tessahoad.co.uk>
2025-12-01 22:40:43 +0000

    Part Two

--- src/bin/dayone.rs
+++ src/bin/dayone.rs
@@ -5,18 +5,32 @@ static INPUT: &str = include_str!("../input/dayone.txt");
 // struct for dial
 #[derive(Debug, Copy, Clone)]
 struct Dial {
-    position: i32
+    current_position: i32,
+    zero_clicks: i32
 }
 
 // impl that applies an instruction
 impl Dial {
     fn apply(&mut self, instruction: &Instruction) {
         match instruction {
-            Instruction::RotateLeft { degree } =>
-                self.position = ((self.position - degree) % 100 + 100) % 100,
-            Instruction::RotateRight { degree } =>
-                self.position = (self.position + degree) % 100,
-            
+            Instruction::RotateLeft { degree } => {
+                let old_position = self.current_position;
+                self.current_position = (self.current_position - degree).rem_euclid(100);
+
+                if old_position == 0 && *degree >= 100  {
+                    self.zero_clicks += degree.div_euclid(100);
+                } else if old_position != 0 && *degree >= old_position {
+                    let remaining = degree - old_position;
+                    self.zero_clicks += remaining.div_euclid(100) + 1
+                }           
+            }
+            Instruction::RotateRight { degree } => {
+                let new_position = self.current_position + degree;
+                self.current_position = new_position.rem_euclid(100);
+
+                let zero_passes = new_position.div_euclid(100);
+                self.zero_clicks += zero_passes.abs();
+            }
         }
     }
 }
@@ -50,31 +64,50 @@ impl FromStr for Instruction {
     }
 }
 
-
 fn part_one() -> Result<(), String> {
     let instructions: Vec<Instruction> = INPUT.lines()
         .map(|line| Instruction::from_str(line))
         .collect::<Result<_, _>>()?;
-    let dial = Dial{ position: 50 };
+    let dial = Dial{ current_position: 50, zero_clicks: 0 };
 
 
-    let states: Vec<Dial> = instructions.iter().scan(dial, |state, instruction| {
+    let dial_states: Vec<Dial> = instructions.iter().scan(dial, |state, instruction| {
         let mut next_state = *state;
         next_state.apply(instruction);
         *state = next_state;
         Some(next_state)
     }).collect();
 
-    let zeros = states.iter().filter(|d| d.position == 0).count();
+    let zeros = dial_states.iter().filter(|d| d.current_position == 0).count();
     println!("Position is 0 {} times", zeros);
 
-    println!("Final dial position: {}", dial.position);
+    println!("Final dial position: {}", dial.current_position);
     Ok(())
 }
 
+fn part_two() -> Result<(), String> {
+    let instructions: Vec<Instruction> = INPUT.lines()
+        .map(|line| Instruction::from_str(line))
+        .collect::<Result<_, _>>()?;
+    let dial = Dial{ current_position: 50, zero_clicks: 0 };
+    
+    let dial_states: Vec<Dial> = instructions.iter().scan(dial, |state, instruction| {
+        let mut next_state = *state;
+        next_state.apply(instruction);
+        *state = next_state;
+        Some(next_state)
+    }).collect();
+
+
+    let final_state = dial_states.last().unwrap();
+
+    println!("Final zero clicks: {}", final_state.zero_clicks);
+    Ok(())
+}
 
 fn main() -> Result<(), String> {
-    return part_one();
+    let _ = part_one();
+    return part_two();
 }
 
 #[cfg(test)]
@@ -126,6 +159,160 @@ mod tests {
         // When
         // Then
         assert_eq!(Instruction::from_str(input), Err("Failed to parse degree"))
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_no_zero_pass() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 45 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 5);
+        assert_eq!(dial.zero_clicks, 0);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_no_zero_pass_and_zero_start() {
+        // Given
+        let mut dial = Dial { current_position: 0, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 45 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 55);
+        assert_eq!(dial.zero_clicks, 0);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_land_on_zero() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 350 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 0);
+        assert_eq!(dial.zero_clicks, 4);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_land_on_nonzero_with_diff_greater_than_100() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 355 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 95);
+        assert_eq!(dial.zero_clicks, 4);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_start_nonzero_and_land_on_nonzero_with_diff_less_than_100() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 55 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 95);
+        assert_eq!(dial.zero_clicks, 1);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_start_nonzero_and_land_on_zero() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 150 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 0);
+        assert_eq!(dial.zero_clicks, 2);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_left_rotations_when_start_zero_and_land_on_zero() {
+        // Given
+        let mut dial = Dial { current_position: 0, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateLeft { degree: 200 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 0);
+        assert_eq!(dial.zero_clicks, 2);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_right_rotations_when_start_nonzero_and_land_on_zero() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateRight{ degree: 355 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 5);
+        assert_eq!(dial.zero_clicks, 4);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_right_rotations_when_land_on_nonzero() {
+        // Given
+        let mut dial = Dial { current_position: 50, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateRight{ degree: 350 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 0);
+        assert_eq!(dial.zero_clicks, 4);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_right_rotations_when_start_on_zero_and_land_nonzero() {
+        // Given
+        let mut dial = Dial { current_position: 0, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateRight{ degree: 350 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 50);
+        assert_eq!(dial.zero_clicks, 3);
+    }
+
+    #[test]
+    fn test_zero_clicks_added_for_right_rotations_when_start_on_zero_and_land_zero() {
+        // Given
+        let mut dial = Dial { current_position: 0, zero_clicks: 0 }; 
+        let instruction = Instruction::RotateRight{ degree: 400 };
+
+        // When
+        dial.apply(&instruction);
+        
+        // Then
+        assert_eq!(dial.current_position, 0);
+        assert_eq!(dial.zero_clicks, 4);
     }
 
 }
